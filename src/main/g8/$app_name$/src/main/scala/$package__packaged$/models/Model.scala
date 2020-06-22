6b2f75ca108fd77d959c9b6ad6d49816407e6c50
jlcampbell1991@gmail.com <jlcampbell1991@gmail.com>
2020-06-22 15:09:31 -0500

    LocalDate -> LocalDateTime

--- src/main/g8/$app_name$/src/main/scala/$package__packaged$/models/Model.scala
+++ src/main/g8/$app_name$/src/main/scala/$package__packaged$/models/Model.scala
@@ -2,7 +2,7 @@ package $package$
 
 import cats.effect.Sync
 import doobie._
-import java.time.{LocalDate, Month}
+import java.time.{LocalDateTime, Month}
 import java.util.UUID
 import org.http4s._
 import org.http4s.UrlForm
@@ -14,14 +14,17 @@ trait Model {
   protected def getValueOrRaiseError[F[_]: Sync](form: UrlForm, value: String): F[String] =
     form
       .getFirst(value)
-      .fold(Sync[F].raiseError[String](MalformedMessageBodyFailure(s"forgot \$value")))(Sync[F].pure(_))
+      .fold(Sync[F].raiseError[String](MalformedMessageBodyFailure(s"forgot $value")))(Sync[F].pure(_))
 }
 
-case class Date(value: LocalDate)
+case class Date(value: LocalDateTime)
 
-object Date extends doobie.util.meta.LegacyLocalDateMetaInstance {
-  def apply(month: Int, day: Int, year: Int) = LocalDate.of(year, Month.of(month), day)
-  def now: Date = Date(LocalDate.now)
-  implicit val get: Get[Date] = Get[LocalDate].map(Date(_))
-  implicit val put: Put[Date] = Put[LocalDate].contramap(_.value)
+object Date extends doobie.util.meta.TimeMetaInstances with doobie.util.meta.MetaConstructors {
+  def apply(month: Int, day: Int, year: Int) =
+    LocalDateTime.of(year, Month.of(month), day, now.value.getHour, now.value.getMinute)
+
+  def now: Date = Date(LocalDateTime.now)
+
+  implicit val get: Get[Date] = Get[LocalDateTime].map(Date(_))
+  implicit val put: Put[Date] = Put[LocalDateTime].contramap(_.value)
 }

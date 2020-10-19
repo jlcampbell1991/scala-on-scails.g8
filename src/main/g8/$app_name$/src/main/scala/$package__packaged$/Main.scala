88469ad4074acb17123be28919cb536fe1ee005d
jlcampbell1991 <jlcampbell1991@gmail.com>
2020-10-19 13:49:34 -0500

    main and gitignore docker prep

--- src/main/g8/$app_name$/src/main/scala/$package__packaged$/Main.scala
+++ src/main/g8/$app_name$/src/main/scala/$package__packaged$/Main.scala
@@ -6,13 +6,6 @@ import cats.effect.{ExitCode, IO, IOApp}
 import DBDriver.XA
 
 object Main extends IOApp {
-  def run(args: List[String]) = args match {
-    case Nil => SetupServer.stream[IO].compile.drain.as(ExitCode.Success)
-    case _ =>
-      (args(0), args(1)) match {
-        case ("dev", "initialize") => DB.initialize(DBDriver.development).sequence.as(ExitCode.Success)
-        case ("prod", "initialize") => DB.initialize(DBDriver.production).sequence.as(ExitCode.Success)
-        case (_, _) => SetupServer.stream[IO].compile.drain.as(ExitCode.Success)
-      }
-  }
+  def run(args: List[String]) =
+    SetupServer.stream[IO].compile.drain.as(ExitCode.Success)
 }
